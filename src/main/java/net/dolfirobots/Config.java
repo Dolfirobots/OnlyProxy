8f9c8db698dda91e02a238ffcc5718caba51e246
Dolfrobots <127861291+Dolfirobots@users.noreply.github.com>
2025-10-08 14:43:35 +0200

    Update Config.java

--- src/main/java/net/dolfirobots/Config.java
+++ src/main/java/net/dolfirobots/Config.java
@@ -17,7 +17,7 @@ public class Config {
         if (getString("prefix").isEmpty() || getString("prefix") == null) prefix = "&7[&eOnlyProxy&7] ";
         return prefix;
     }
-    // I dont wanne talk what there is
+    // I dont wanne talk how dump the lines under this comment are (you can easy do "Config.getConfig().getString("prefix")" for example)
     // I just want to code
     public static String getString(String path) {
         return getConfig().getString(path);
